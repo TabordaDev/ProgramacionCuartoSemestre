3baaeda239515defb1d5dbe7e6dec636b3cfc2a4
TabordaDev <jdavidtaborda@estudiante.uniajc.edu.co>
2025-08-12 17:35:30 +0000

    creacion de la clase OperacionesArreglos

---
+++ EjecutarOperacionesA.java
@@ -0,0 +1,14 @@
+public class EjecutarOperacionesA {
+    public static void main(String[] args){
+
+        OperacionesArreglos objOp = new OperacionesArreglos();
+
+        //Llenar el arreglo
+        int[] x = objOp.llenarArregloEnteros(4);
+
+        //Mostrar el arreglo
+        objOp.mostrarArregloEnteros(x);
+
+        System.out.println(objOp.promedioArregloEnteros(x));
+    }
+}
